b92de0c7e1eec44a9b45429ab3582f66d035d50b
ik5 <idok@linesip.com>
2013-01-19 21:43:23 +0200

    Starting to work on the extension

---
+++ src/untlazsplitview_code.pas
@@ -0,0 +1,27 @@
+unit untLazSplitView_Code;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, SrcEditorIntf;
+
+type
+  TTabInfo = record
+    ActiveEditor : TSourceEditorInterface;
+    ActiveWindow    :
+  end;
+
+procedure register;
+
+implementation
+uses MenuIntf, IDECommands, LCLType;
+
+procedure register;
+begin
+
+end;
+
+end.
+
