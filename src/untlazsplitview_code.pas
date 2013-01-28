7e0b87bd5c8950d99197e9d2ca0122abc8c5ad44
ik5 <idok@linesip.com>
2013-01-28 18:08:15 +0200

    Trying to make a prototype menu item, and key registration support

--- src/untlazsplitview_code.pas
+++ src/untlazsplitview_code.pas
@@ -24,9 +24,28 @@ procedure register;
 implementation
 uses MenuIntf, IDECommands, LCLType;
 
-procedure register;
-begin
+resourcestring
+  txtSplitViewPlugins           = 'Split View';
+  txtToggleSplitView            = 'Toggle Split View';
+  txtToggleSplitViewDescription = 'Allow the toggle between split view of the active editor';
 
+procedure register;
+var
+  Key, Key2 : TIDEShortCut;
+  Cat       : TIDECommandCategory;
+  Cmd       : TIDECommand;
+begin
+  RegisterIDEMenuCommand(SrcEditMenuSectionPages, 'SplitViewSep', '-');
+
+  Key  := IDEShortCut(VK_UNKNOWN,[],VK_UNKNOWN,[]);
+  Key2 := Key;
+  Cat  := IDECommandList.FindCategoryByName(txtSplitViewPlugins);
+  Cmd  := RegisterIDECommand(Cat, txtToggleSplitView,
+                             txtToggleSplitViewDescription,
+                             Key, Key2, nil, nil{@SplitSelect});
+
+  RegisterIDEMenuCommand(SrcEditMenuSectionPages, 'SplitViewToggle',
+    txtToggleSplitView, nil, {@SplitSelect}nil, Cmd);
 end;
 
 end.
