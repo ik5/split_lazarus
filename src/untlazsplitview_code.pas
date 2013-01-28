13203575d2707adc5834d79d743e63a2fd10879b
ik5 <idok@linesip.com>
2013-01-28 21:12:13 +0200

    Added license, also changed logic. now it suppose to really be toggled ...

--- src/untlazsplitview_code.pas
+++ src/untlazsplitview_code.pas
@@ -1,3 +1,24 @@
+{
+
+Copyright (c) 2012-2013 Ido Kanner idokan at@at gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+}
 unit untLazSplitView_Code;
 
 {$mode objfpc}{$H+}
@@ -135,11 +156,28 @@ procedure TSplitView.ToggleSplitView(Vert : Boolean);
 var ActiveEditor : TSourceEditorInterface;
     index        : integer;
     tab          : TTabInfo;
+
+ procedure Hide;
+ begin
+   tab.SplitEditor.Visible := false;
+   tab.SplitEditor.Free;
+   tab.Splitter.Visible    := false;
+   tab.Splitter.Free;
+ end;
+
 begin
   ActiveEditor := SourceEditorManagerIntf.ActiveEditor;
   index        := FTabList.IndexOf(ActiveEditor);
   if index > -1 then
-    tab := PTabInfo(FTabList.Items[index])^
+    begin
+      tab := PTabInfo(FTabList.Items[index])^;
+      Hide;
+		  case tab.SplitType of // nothing more to do if the item is already the
+                            // same as it was. "Toggle" will close it only ...
+		    stVert : if     Vert then exit;
+        stHorz : if not Vert then exit;
+      end;
+    end
   else begin
     tab.ActiveEditor := ActiveEditor;
     tab.SplitEditor  := Nil;
